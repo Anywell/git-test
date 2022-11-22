a6826a6e2c927079b542b7d083d5bdfb1481c9be
anywell <apple52500@126.com>
2022-11-22 17:14:57 +0800

    master fourth commit.

--- src/main/java/com/any/git/GitTest.java
+++ src/main/java/com/any/git/GitTest.java
@@ -10,4 +10,11 @@ public class GitTest {
     public int add(int a, int b){
         return a + b;
     }
+
+
+    public int sub(int a, int b){
+        int c =  a - b;
+        return c;
+    }
+
 }
